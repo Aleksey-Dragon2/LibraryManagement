8a1ccae5bc7bee43bd7f3b860a666539a65fbe31
Dragon <aleha2552654@gmail.com>
2025-10-29 00:54:40 +0300

    add(Abstractions.IAuthorService)

---
+++ Application/Abstractions/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<Author>> GetAllAsync();
+        Task<Author> GetAsync(int id);
+        Task<Author> CreateAsync(Author author);
+        Task<Author> UpdateAsync(int id, Author author);
+        Task<bool> DeleteAsync(int id);
+    }
+}
