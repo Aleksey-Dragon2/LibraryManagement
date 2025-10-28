60a56cabc52144858f815e196ec1faec8432ecce
Dragon <aleha2552654@gmail.com>
2025-10-29 00:54:31 +0300

    add(Abstractions.IBookService)

---
+++ Application/Abstractions/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Abstractions
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetAsync(int id);
+        Task<Book> CreateAsync(Book book);
+        Task<Book> UpdateAsync(int id, Book book);
+        Task<bool> DeleteAsync(int id);
+    }
+}
