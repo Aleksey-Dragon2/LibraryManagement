c47e0635d8979bb239293171f79cce63f6e9284a
Dragon <aleha2552654@gmail.com>
2025-10-29 00:55:22 +0300

    add(Services.BookService)

---
+++ Application/Services/BookService.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class BookService
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _bookRepository.GetAllAsync();
+        }
+
+        public async Task<Book> GetAsync(int id)
+        {
+            return await _bookRepository.GetAsync(id);
+        }
+
+        public async Task<Book> CreateAsync(Book book)
+        {
+            return await _bookRepository.CreateAsync(book);
+        }
+
+        public async Task<Book> UpdateAsync(int id, Book book)
+        {
+            return await _bookRepository.UpdateAsync(id, book);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _bookRepository.DeleteAsync(id);
+        }
+    }
+}
