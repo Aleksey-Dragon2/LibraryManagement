a28c7cd0a69fa2701a1eca810897d5010a9d7fe0
Dragon <aleha2552654@gmail.com>
2025-10-28 22:44:06 +0300

    add(Domain.Abstractions) IAuthorRepository

---
+++ Domain/Abstractions/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetAsync(int id);
+        Task<Book> CreateAsync(Book book);
+        Task<Book> UpdateAsync(Book book);
+        Task<bool> DeleteAsync(int id);
+    }
+}
