c64e01663b1a3e556a2f06d6f4ffeb36744f47cf
Dragon <aleha2552654@gmail.com>
2025-10-28 23:38:26 +0300

    add(Abstractions.IBookRepository)

---
+++ Infrastructure/DB/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.DB
+{
+    public class ApplicationContext
+    {
+        public List<Author> Authors { get; set; }
+
+        public List<Book> Books { get; set; }
+
+        public ApplicationContext()
+        {
+            Authors = new List<Author>();
+            Books = new List<Book>();
+        }
+    }
+}
