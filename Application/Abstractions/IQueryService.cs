286e90c6e229fc520bcb3d20340965c09daa619e
Dragon <aleha2552654@gmail.com>
2025-10-30 03:30:04 +0300

    add(IQueryService)

---
+++ Application/Abstractions/IQueryService.cs
@@ -0,0 +1,13 @@
+﻿
+using Domain.Entities;
+
+namespace Application.Abstractions
+{
+    public interface IQueryService
+    {
+        Task<IEnumerable<Author>> GetAllAuthorsWithCountBooksAsync(int countBooks);
+        Task<IEnumerable<Author>> GetAllAuthorsStartWithAsync(string nameStart);
+        Task<IEnumerable<Book>> GetAllBooksByAuthorSortedByYearAsync(int authorId);
+        Task<IEnumerable<Book>> GetAllBookPublishedAfterYearAsync(int year);
+    }
+}
